1c5d2044de925ed43307fd0738c4ab30709a0025
thediep094 <thediep094@gmail.com>
2023-10-18 01:09:33 +0700

    update

--- src/common/apiUrl.js
+++ src/common/apiUrl.js
@@ -1,5 +1,5 @@
-// export const apiUrl = `https://thaoqltv-back.vercel.app/api`;
-// export const apiImages = `https://thaoqltv-back.vercel.app/images`;
+export const apiUrl = `https://thaoqltv-back.vercel.app/api`;
+export const apiImages = `https://thaoqltv-back.vercel.app/images`;
 
-export const apiUrl = `http://localhost:3000/api`;
-export const apiImages = `http://localhost:3000/images`;
+// export const apiUrl = `http://localhost:3000/api`;
+// export const apiImages = `http://localhost:3000/images`;
